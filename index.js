fedbfd9ca5e06be947b3d37454c5e0b6e27843e7
Richie Bendall <richiebendall@gmail.com>
2020-01-17 06:55:11 +1300

    refactor: Update deps
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -8,98 +8,98 @@ const roundTo = require("round-to")
 const mapObject = require("map-obj")
 
 module.exports = async (amount, from, to, { date = "latest", precision = 2, provider = "exchangeratesapi", apiKey } = {}) => {
-    // `amount` must be a number
-    ow(amount, ow.number)
+	// `amount` must be a number
+	ow(amount, ow.number)
 
-    // `from` must be a string of length 3
-    ow(from, ow.string.length(3))
+	// `from` must be a string of length 3
+	ow(from, ow.string.length(3))
 
-    // `to` must be a string of length 3
-    ow(to, ow.string.length(3))
+	// `to` must be a string of length 3
+	ow(to, ow.string.length(3))
 
-    // `date` must be "latest" or compatible with dayjs
-    ow(date, ow.any(
-        ow.string.is((value) => value === "latest" || day(value).format() !== "Invalid Date")),
-    ow.number.is((value) => day(value).format() !== "Invalid Date"),
-    ow.date.is((value) => day(value).format() !== "Invalid Date"),
-    ow.object.is((value) => day(value).format() !== "Invalid Date"), // Class
-    )
+	// `date` must be "latest" or compatible with dayjs
+	ow(date, ow.any(
+		ow.string.is((value) => value === "latest" || day(value).format() !== "Invalid Date")),
+	ow.number.is((value) => day(value).format() !== "Invalid Date"),
+	ow.date.is((value) => day(value).format() !== "Invalid Date"),
+	ow.object.is((value) => day(value).format() !== "Invalid Date"), // Class
+	)
 
-    // `precision` must be a number
-    ow(precision, ow.number)
+	// `precision` must be a number
+	ow(precision, ow.number)
 
-    // `provider` must be a string
-    ow(provider, ow.string.is((value) => ["exchangeratesapi", "fixer", "currencylayer", "openexchangerates"].includes(value)))
+	// `provider` must be a string
+	ow(provider, ow.string.is((value) => ["exchangeratesapi", "fixer", "currencylayer", "openexchangerates"].includes(value)))
 
-    // Convert `from` and `to` to uppercase
-    from = from.toUpperCase()
-    to = to.toUpperCase()
+	// Convert `from` and `to` to uppercase
+	from = from.toUpperCase()
+	to = to.toUpperCase()
 
-    // If `date` is not latest, parse it
-    if (date !== "latest") date = day(date).format("YYYY-MM-DD")
+	// If `date` is not latest, parse it
+	if (date !== "latest") date = day(date).format("YYYY-MM-DD")
 
-    // Conversion `options` object
-    const options = {
-        from,
-        to,
-        base: from,
-        rates: {},
-    }
+	// Conversion `options` object
+	const options = {
+		from,
+		to,
+		base: from,
+		rates: {},
+	}
 
-    if (provider === "exchangeratesapi") {
-        // Using api.exchangeratesapi.io
-        const { rates } = await ky(`https://api.exchangeratesapi.io/${date}`, {
-            searchParams: {
-                base: from,
-                symbols: to,
-            },
-        }).json()
-        options.rates = rates
-    } else if (provider === "fixer") {
-        // Using data.fixer.io
-        ow(apiKey, ow.string)
-        const { rates, base } = await ky(`http://data.fixer.io/api/${date}`, {
-            searchParams: {
-                access_key: apiKey,
-                symbols: [from, to],
-            },
-        }).json()
-        options.rates = rates
-        options.base = base
-    } else if (provider === "currencylayer") {
-        // Using apilayer.net
-        ow(apiKey, ow.string)
-        const { quotes: rates, source: base } = date === "latest" ? await ky("http://apilayer.net/api/live", {
-            searchParams: {
-                access_key: apiKey,
-                currencies: [from, to],
-            },
-        }).json() : await ky("http://apilayer.net/api/historic", {
-            searchParams: {
-                access_key: apiKey,
-                currencies: [from, to],
-                date,
-            },
-        }).json()
-        options.rates = mapObject(rates, (key, value) => [key.slice(3), value])
-        options.base = base
-    } else if (provider === "openexchangerates") {
-        // Using openexchangerates.org
-        ow(apiKey, ow.string)
-        const { rates, base } = await ky(date === "latest" ? "https://openexchangerates.org/api/latest.json" : `https://openexchangerates.org/api/historical/${date}.json`, {
-            searchParams: {
-                app_id: apiKey,
-                symbols: [from, to],
-                prettyprint: false,
-            },
-        }).json()
-        options.rates = rates
-        options.base = base
-    }
+	if (provider === "exchangeratesapi") {
+		// Using api.exchangeratesapi.io
+		const { rates } = await ky(`https://api.exchangeratesapi.io/${date}`, {
+			searchParams: {
+				base: from,
+				symbols: to,
+			},
+		}).json()
+		options.rates = rates
+	} else if (provider === "fixer") {
+		// Using data.fixer.io
+		ow(apiKey, ow.string)
+		const { rates, base } = await ky(`http://data.fixer.io/api/${date}`, {
+			searchParams: {
+				access_key: apiKey,
+				symbols: [from, to],
+			},
+		}).json()
+		options.rates = rates
+		options.base = base
+	} else if (provider === "currencylayer") {
+		// Using apilayer.net
+		ow(apiKey, ow.string)
+		const { quotes: rates, source: base } = date === "latest" ? await ky("http://apilayer.net/api/live", {
+			searchParams: {
+				access_key: apiKey,
+				currencies: [from, to],
+			},
+		}).json() : await ky("http://apilayer.net/api/historic", {
+			searchParams: {
+				access_key: apiKey,
+				currencies: [from, to],
+				date,
+			},
+		}).json()
+		options.rates = mapObject(rates, (key, value) => [key.slice(3), value])
+		options.base = base
+	} else if (provider === "openexchangerates") {
+		// Using openexchangerates.org
+		ow(apiKey, ow.string)
+		const { rates, base } = await ky(date === "latest" ? "https://openexchangerates.org/api/latest.json" : `https://openexchangerates.org/api/historical/${date}.json`, {
+			searchParams: {
+				app_id: apiKey,
+				symbols: [from, to],
+				prettyprint: false,
+			},
+		}).json()
+		options.rates = rates
+		options.base = base
+	}
 
-    // Convert currency
-    const converted = convert(amount, options)
+	// Convert currency
+	const converted = convert(amount, options)
 
-    // Round according to precision
-    return precision === Infinity ? converted : roundTo(converted, precision)
+	// Round according to precision
+	return roundTo(converted, precision)
 }
--- test.js
+++ test.js
@@ -3,6 +3,6 @@ import is from "@sindresorhus/is"
 import casha from "."
 
 test("main", async (t) => {
-    t.true(is.number(await casha(10, "nzd", "usd")))
-    t.true(is.number(await casha(10, "nzd", "usd", { date: "2019-11-12" })))
+	t.true(is.number(await casha(10, "nzd", "usd")))
+	t.true(is.number(await casha(10, "nzd", "usd", { date: "2019-11-12" })))
 })
